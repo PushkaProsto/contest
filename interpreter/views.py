8c71ccdd013b5c212d8ffe4bc360f423882b8670
pythonMaks <122172870+pythonMaks@users.noreply.github.com>
2023-04-15 13:26:07 +0600

    Update views.py

--- interpreter/views.py
+++ interpreter/views.py
@@ -8,7 +8,7 @@ def index(request):
     if request.method == 'POST':        
         code = request.POST['interpreter']
         process = Popen(['python', '-c', code], stdin=PIPE, stdout=PIPE, stderr=PIPE)
-        output, error = process.communicate(input=input_data[i].encode())
+        output, error = process.communicate(input=input_data[i].encode()
         
         context = {            
         }
@@ -20,7 +20,7 @@ def index(request):
             pass
         try:
             encoding = chardet.detect(error)['encoding']
-            contest['error'] = error.decode(encoding).strip())
+            contest['error'] = error.decode(encoding).strip()
         except:
             pass   
     
